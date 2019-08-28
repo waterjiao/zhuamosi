419b68895b4e92a6be9dc9aa3a133e673b92fe37
menger <785672729@qq.com>
2019-08-28 23:51:17 +0800

    add h2 and mybatis

--- src/main/java/com/zhua/mosi/controller/AuthorizeController.java
+++ src/main/java/com/zhua/mosi/controller/AuthorizeController.java
@@ -3,12 +3,17 @@ package com.zhua.mosi.controller;
 
 import com.zhua.mosi.dto.AccessTokenDTO;
 import com.zhua.mosi.dto.GithubUser;
+import com.zhua.mosi.mapper.UserMapper;
+import com.zhua.mosi.model.User;
 import com.zhua.mosi.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -23,9 +28,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -34,7 +43,25 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        System.out.println(githubUser.getName());
+        if (githubUser!=null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //login success
+            request.getSession().setAttribute("githubuser",githubUser);
+            return "redirect:index";
+        } else {
+            //login fail
+        }
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index(){
         return "index";
     }
 }
