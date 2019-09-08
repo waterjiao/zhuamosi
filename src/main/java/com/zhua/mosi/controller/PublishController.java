118076e33fbe145cd22ed9c5d3ec50680c021cb8
menger <785672729@qq.com>
2019-09-08 22:09:16 +0800

    add question

--- src/main/java/com/zhua/mosi/controller/PublishController.java
+++ src/main/java/com/zhua/mosi/controller/PublishController.java
@@ -1,12 +1,82 @@
 package com.zhua.mosi.controller;
 
+import com.zhua.mosi.mapper.QuestionMapper;
+import com.zhua.mosi.mapper.UserMapper;
+import com.zhua.mosi.model.Question;
+import com.zhua.mosi.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
     @GetMapping("/publish")
     public String publish(){
         return "publish";
+    }
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam(value = "title") String title,
+            @RequestParam(value = "description") String description,
+            @RequestParam(value = "tag") String tag,
+            HttpServletRequest request,
+            Model model
+    ){
+
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                user = userMapper.findBytoken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        if(user==null){
+            model.addAttribute("error","用户未登录");
+            return "publish";
+        }
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+        if (title==null||title == ""){
+            model.addAttribute("error","标题不能为空");
+            return "publish";
+        }
+        if (description==null||description==""){
+            model.addAttribute("error","问题补充不能为空");
+            return "publish";
+        }
+        if (tag==null||tag==""){
+            model.addAttribute("error","标签不能为空");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+
+        questionMapper.create(question);
+        return "redirect:/";
     }
 }
