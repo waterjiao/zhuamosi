a6c1b79349794bffc86e7b3b47f308af54d9ed5e
menger <785672729@qq.com>
2019-09-13 16:45:30 +0800

    add lombok

--- src/main/java/com/zhua/mosi/model/User.java
+++ src/main/java/com/zhua/mosi/model/User.java
@@ -1,5 +1,8 @@
 package com.zhua.mosi.model;
 
+import lombok.Data;
+
+@Data
 public class User {
     private Integer id;
     private String name;
@@ -7,62 +10,5 @@ public class User {
     private String token;
     private Long gmtCreate;
     private Long gmtModified;
-
-    public String getAvatorUrl() {
-        return avatorUrl;
-    }
-
-    public void setAvatorUrl(String avatorUrl) {
-        this.avatorUrl = avatorUrl;
-    }
-
     private String avatorUrl;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(String accountId) {
-        this.accountId = accountId;
-    }
-
-    public Long getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Long gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Long getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Long gmtModified) {
-        this.gmtModified = gmtModified;
-    }
 }
